430ccc1bbeaa445a12bf09a8384629137814d3c3
KuoAiTe <robinsa87@gmail.com>
2019-11-12 01:23:08 -0600

    Fixed bugs and did code optimization

--- aite.js
+++ aite.js
@@ -1,212 +1,199 @@
+/**
+ * Global settings
+ * @author Ai-Te Kuo
+ */
 var studentInSection = new Set();
 var forceFliter = false;
 var autoFilter = false;
 var autoShowGrade = false;
 var pageType = 0;
 var lock = false;
-var showUI = false;
 var disable = false;
-function getStudentList(){
-	var studentList;
-	var form = document.getElementById("webcat_Form_3");
-	if(form){
-		var parentNode = form.getElementsByTagName("tbody")[2];
-		studentList = parentNode.getElementsByTagName("tr");
-	}
-	return studentList;
-}
-function removeStudents(){
-	var studentList = getStudentList();
-	clean(studentList);
-}
 
-function getStudentName(studentName){
-	var re = / +\(.+\)/;
-	var pureName = studentName.replace(re, '');
-	return pureName;
+/**
+ * Feteh the form and remove all the information except the student list.
+ * @return the student list DOM
+ */
+function getStudentList() {
+  var studentList = null;
+  var form = $("#webcat_Form_3");
+  var formExist = form.length > 0;
+  if (formExist) {
+    var parentNode = form.find("tbody:nth-child(2)");
+    studentList = parentNode.find("tr");
+  }
+  return studentList;
+}
+/**
+ * Aite, Kuo
+ * Aite, Kuo (azkxxxx0000)
+ * Aite, Kuo (azk000)    try #1
+ * The function should return Aite Kuo with all of the above examples.
+ * @return last name, first name
+ */
+function getStudentName(tdStudentName) {
+  var re = /[A-Za-z]+, [A-Za-z]+/;
+  var studentName = tdStudentName.match(re);
+  return studentName[0];
 }
 
 function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
-async function autoCheckGrade(){
-	if(autoShowGrade){
-		var attempts = 0;
-		var checkBox = $('input#done');
-		while(checkBox.length == 0 && attempts ++ < 100){
-			await timeout(30);
-			checkBox = $('input#done');
-		}
-		var pressed = checkBox.attr('aria-pressed');
-		if(!pressed)
-			checkBox.click();
-	}
+/**
+ * Automatically check "Show grade to student?" if enabled.
+ * 
+ */
+async function autoCheckGrade() {
+  if (!autoShowGrade || disable) return;
+  var attempts = 0;
+  var checkBox;
+  while (checkBox = $('input#done'), checkBox.length == 0 && attempts++ < 500)
+    await timeout(10);
+  var pressed = checkBox.attr('aria-pressed');
+  if (!pressed)
+    checkBox.click();
 }
-async function async_refresh2(){
-	var studentList = getStudentList();
-	var old_name = studentList[0].innerText;
-	var new_name = old_name;
-	var attempts = 0;
-	while(attempts ++ < 100){
-		await timeout(30);
-		studentList = getStudentList();
-		new_name = studentList[0].innerText;
-		if(old_name != new_name) 
-			break;
-	}
-	await refreshStudents(studentList);
+/**
+ * Listen to the page and refresh the table when the table is loaded.
+ */
+async function oneTimeTableRefreshListener() {
+  var attempts = 0;
+  var studentList;
+  do {
+    studentList = getStudentList();
+    await timeout(10);
+  } while ((studentList == null || studentList.length == 0) && attempts++ < 1000);
+  await refreshTable(studentList);
 }
-async function async_refresh(){
-	await timeout(100);
-	studentList = getStudentList();
-	var attempts = 0;
-	if(studentList){
-		studentList = null;
-	}
-	while(!studentList && attempts ++ < 100){
-		await timeout(30);
-		studentList = getStudentList();
-	}
-	await refreshStudents(studentList);
-}
-async function refreshStudents(studentList){
-	if(studentList){
-		var td_student_dom, student_info, td_student_name;
-		var tr;
-		var lockClass = 'unlock';
-		for (var i = 0, len = studentList.length; i < len; i++) {
-			tr = studentList[i];
-			student_info = tr.getElementsByTagName("td");
-			if(student_info.length > 2) {
-				td_student_dom = student_info[1];
-				td_student_name = td_student_dom.innerText;
-				if(lock) lockClass = 'locked';
-				if(isStudentInSection(getStudentName(td_student_name)))
-					td_student_dom.innerHTML = `<span class="inSection `+ lockClass + `">` + td_student_name + `</span>`;
-				else
-					if(autoFilter)
-						tr.style.display = 'none';
-					else {
-						tr.style.display = 'table-row';
-						td_student_dom.innerHTML = `<span class="outSection `+ lockClass + `">` + td_student_name + `</span>`;
-					}
-			}
 
-		}
-	}
-}
-function isStudentInSection(student_name){
-		return studentInSection.has(student_name);
-}
-function handleStudent(student){
-	if(!lock){
-		var classAssigned = '';
-		var name = student[0].innerText;
-		var pureName = getStudentName(name);
-		if( isStudentInSection(pureName) ){
-			studentInSection.delete(pureName);
-			classAssigned = 'outSection';
-		} else{
-			studentInSection.add(pureName);
-			classAssigned = 'inSection'
-		}
-		chrome.storage.sync.set({
-	    studentInSection: Array.from(studentInSection)
-	  }, function() {
-	  	student.removeClass('inSection').removeClass('outSection').addClass(classAssigned);
-	  });
-	}
-}
-$( document ).ready(function() {
+/**
+ * Refresh the table.
+ * If the student is in the set, then assign class attribute "inSection"
+ * Otherwise, "outSection"
+ * @param studentList an array of DOM objects
+ */
+async function refreshTable(studentList) {
+  if (studentList) {
+    var tdStudentDOM, student_info, tdStudentName, studentName;
+    var tr;
+    var classAttribute = 'unlock';
+    var inSection;
+    for (var i = 0, len = studentList.length; i < len; i++) {
+      inSection = false;
+      tr = studentList[i];
+      student_info = tr.getElementsByTagName("td");
+      if (student_info.length > 2) {
+        tdStudentDOM = student_info[1];
+        tdStudentName = tdStudentDOM.innerText;
+        studentName = getStudentName(tdStudentName);
+        inSection = isStudentInSection(studentName);
+        classAttribute = (lock) ? "locked " : "unlock ";
+        classAttribute += (inSection) ? "inSection" : "outSection";
+        if (disable)
+          tdStudentDOM.innerHTML = `<span>` + tdStudentName + `</span>`;
+        else
+          tdStudentDOM.innerHTML = `<span class=" ${classAttribute}">` + tdStudentName + `</span>`;
+        tr.style.display = (autoFilter && !inSection && !disable) ? 'none' : 'table-row';
+      }
 
-	chrome.storage.sync.get({
-		showUI: true,
-		disable: false
-		}, function(items){
-			showUI = items.showUI;
-			disable = items.disable;
-			if(!disable){
-				var title = document.title;
-				if(title == "View Submissions"){
-					pageType = 1;
-				}
-				if(title == "Grade One Submission")
-					pageType = 2;
-				if (pageType != 0 ){
-					if(pageType == 1){
-				  	$('html').prepend(`<div class="aiteBtn info"><input name="autoFilter" type="checkbox">AutoFilter   <input name="lock" type="checkbox">Lock</div>`);
-						if(!showUI)
-							$('.aiteBtn').hide();
-						$( document ).on( "change", "input[name=autoFilter]", function() {
-						  autoFilter = $(this).is(':checked');
-							chrome.storage.sync.set({
-						    autoFilter: autoFilter
-						  }, function() {
-						  	async_refresh();
-						  });
-						});
-						$( document ).on( "change", "input[name=lock]", function() {
-						  lock = $(this).is(':checked');
-							chrome.storage.sync.set({
-						    lock: lock
-						  }, function() {
-						  	async_refresh();
-						  });
-						});
-				  	$( document ).on( "click", "span.inSection", function() {
-						  handleStudent($(this));
-						});
-						$( document ).on( "click", "span.outSection", function() {
-						  handleStudent($(this));
-						});
-						$( document ).on( "click", "a.active", function() {
-						  async_refresh2();
-						});
-						$( document ).on( "click", "input.icon", function() {
-						  async_refresh2();
-						});
-						$( document ).on( "change", "table", function() {
-							async_refresh();
-						});
-						chrome.storage.sync.get({
-							studentInSection: [],
-							autoFilter: false,
-							lock: false
-							}, function(items){
-								autoFilter = items.autoFilter;
-								lock = items.lock;
-								$('input[name=autoFilter]').prop('checked', autoFilter);
-								$('input[name=lock]').prop('checked', lock);
-								var temp = items.studentInSection;
-								for ( var i = 0; i < temp.length; i++)
-									studentInSection.add(temp[i]);
-						  	async_refresh();
+    }
+  }
+}
+/**
+ * Check if the student is in the set
+ * @param studentName format -> last name, first name
+ * @return whether the student if in the set.
+ */
+function isStudentInSection(studentName) {
+  return studentInSection.has(studentName);
+}
 
-						});
-				  } else if(pageType == 2) {
-				  	$('html').prepend(`<div class="aiteBtn edit"><input name="autoShowGrade" type="checkbox">AutoShowGradeToStudent</div>`);
-						if(!showUI)
-							$('.aiteBtn').hide();
-						$( document ).on( "change", "input[name=autoShowGrade]", function() {
-						  autoShowGrade = $(this).is(':checked');
-							chrome.storage.sync.set({
-						    autoShowGrade: autoShowGrade
-						  }, function() {
-						  	autoCheckGrade();
-						  });
+/**
+ * When the user hits the student name on the table,
+ * this function will handle it and reassign the DOM attribute.
+ * Afterwards, it saves the new student name set to the cloud.
+ * @param student DOM
+ */
+function toggleStudent(student) {
+  if (lock || disable) return;
+  var tdStudentName = student[0].innerText;
+  var studentName = getStudentName(tdStudentName);
+  var inSection = isStudentInSection(studentName);
+  var lockAttribute = (lock) ? "locked " : "unlock ";
+  var classAttribute = (inSection) ? "outSection" : "inSection";
+  if (inSection)
+    studentInSection.delete(studentName);
+  else
+    studentInSection.add(studentName);
+  // Put the data to the cloud
+  chrome.storage.sync.set({
+    studentInSection: Array.from(studentInSection)
+  }, function() {
+    student.removeClass().addClass(lockAttribute).addClass(classAttribute);
+  });
+}
 
-						});
-						chrome.storage.sync.get({
-							autoShowGrade: false
-							}, function(items){
-								autoShowGrade = items.autoShowGrade;
-								if(autoShowGrade){
-									$('input[name=autoShowGrade]').prop('checked', true);
-									autoCheckGrade();
-								}
-						});
-				  }
-				}
-			}
-	});
+function setUp() {
+  var title = document.title;
+  if (title.includes("View Submissions"))
+    pageType = 1;
+  else if (title.includes("Grade One Submission"))
+    pageType = 2;
+  switch(pageType) {
+    case 1:
+      // Submission Page
+      $(document).on("click", "span.inSection, span.outSection", function() {
+        toggleStudent($(this));
+      });
+      
+      $(document).on("click", "a.active, input.icon", function() {
+        var form = $("#webcat_Form_3 tbody:nth-child(2)").html("");
+        oneTimeTableRefreshListener();
+      });
+      $(document).on("change", "table", function() {
+        var form = $("#webcat_Form_3 tbody:nth-child(2)").html("");
+        oneTimeTableRefreshListener();
+      });
+
+      chrome.storage.sync.get({
+        studentInSection: [],
+      }, function(items) {
+        items.studentInSection.reduce((s, e) => s.add(e), studentInSection);
+        oneTimeTableRefreshListener();
+      });
+  		break;
+  	case 2:
+  	  // Grade Page
+    	if (autoShowGrade)
+      autoCheckGrade();
+    default:
+   		break;
+  }
+}
+$(document).ready(function() {
+  chrome.storage.sync.get({
+    autoFilter: false,
+    lock: false,
+    disable: false,
+    autoShowGrade: false
+  }, function(items) {
+    autoFilter = items.autoFilter;
+    lock = items.lock;
+    disable = items.disable;
+    autoShowGrade = items.autoShowGrade;
+    setUp();
+    oneTimeTableRefreshListener();
+  });
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      disable = request.disable;
+      autoFilter = request.autoFilter;
+      lock = request.lock;
+      autoShowGrade = request.autoShowGrade;
+      if (pageType == 1)
+        oneTimeTableRefreshListener();
+      else if (pageType == 2)
+        autoCheckGrade();
+    });
 });
--- popup.js
+++ popup.js
@@ -1,24 +1,35 @@
 $( document ).ready(function() {
 	chrome.storage.sync.get({
-		showUI: true,
 		disable: false,
+		autoFilter: false,
+		autoShowGrade: false,
+		lock: false,
 		}, function(items){
-			var showUI = items.showUI;
 			var disable = items.disable;
-			$('input[name=showUI]').prop('checked', showUI);
+			var autoFilter = items.autoFilter;
+			var lock = items.lock;
+			var autoShowGrade = items.autoShowGrade;
+			$('input[name=autoFilter]').prop('checked', autoFilter);
+			$('input[name=lock]').prop('checked', lock);
 			$('input[name=disable]').prop('checked', disable);
+			$('input[name=autoShowGrade]').prop('checked', autoShowGrade);
 			
 	});
-	$( document ).on( "change", "input[name=showUI]", function() {
-	  showUI = $(this).is(':checked');
+	$( document ).on( "change", "input[name=autoFilter], input[name=lock], input[name=autoShowGrade], input[name=disable]", function() {
+	  autoFilter = $("input[name=autoFilter]").is(':checked');
+	  lock = $("input[name=lock]").is(':checked');
+	  autoShowGrade = $("input[name=autoShowGrade]").is(':checked');
+	  disable = $("input[name=disable]").is(':checked');
 		chrome.storage.sync.set({
-	    showUI: showUI
-	  }, function() {});
-	});
-	$( document ).on( "change", "input[name=disable]", function() {
-	  disable = $(this).is(':checked');
-		chrome.storage.sync.set({
+	    autoFilter: autoFilter,
+	    lock: lock,
+	    autoShowGrade: autoShowGrade,
 	    disable: disable
-	  }, function() {});
+	  }, function() {
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			  chrome.tabs.sendMessage(tabs[0].id, {autoFilter: autoFilter, lock: lock, autoShowGrade: autoShowGrade, disable: disable}, function(response) {
+			  });
+			});
+	  });
 	});
 });
