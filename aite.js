be3dc57a7d1d4000cbb9ff588265636c28e37d91
KuoAiTe <robinsa87@gmail.com>
2020-01-14 20:00:54 -0600

    fixed some bugs caused from name misformats.'

--- aite.js
+++ aite.js
@@ -9,97 +9,23 @@ var pageType = 0;
 var lock = false;
 var disable = false;
 var lineNumber;
-
+var lastStatus = '';
+const re = /(.+) \(.+\)/;
 /**
  * Feteh the form and remove all the information except the student list.
  * @return the student list DOM
  */
 function getStudentList() {
-  var studentList = null;
-  var form = $("#webcat_Form_3");
-  var formExist = form.length > 0;
+  let studentList = null;
+  const form = $("#webcat_Form_3");
+  const formExist = form.length > 0;
   if (formExist) {
-    var parentNode = form.find("tbody:nth-child(2)");
+    const parentNode = form.find("tbody:nth-child(2)");
     studentList = parentNode.find("tr");
   }
   return studentList;
 }
-/**
- * Aite, Kuo
- * Aite, Kuo (azkxxxx0000)
- * Aite, Kuo (azk000)    try #1
- * The function should return Aite Kuo with all of the above examples.
- * @return last name, first name
- */
-function getStudentName(tdStudentName) {
-  var re = /[A-Za-z]+, [A-Za-z]+/;
-  var studentName = tdStudentName.match(re);
-  return studentName[0];
-}
 
-function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-/**
- * Automatically check "Show grade to student?" if enabled.
- * 
- */
-async function autoCheckGrade() {
-  if (!autoShowGrade || disable) return;
-  var attempts = 0;
-  var checkBox;
-  while (checkBox = $('input#done'), checkBox.length == 0 && attempts++ < 500)
-    await timeout(10);
-  var pressed = checkBox.attr('aria-pressed');
-  if (!pressed)
-    checkBox.click();
-}
-/**
- * Listen to the page and refresh the table when the table is loaded.
- */
-async function oneTimeTableRefreshListener() {
-  var attempts = 0;
-  var studentList;
-  do {
-    studentList = getStudentList();
-    await timeout(10);
-  } while ((studentList == null || studentList.length == 0) && attempts++ < 1000);
-  await refreshTable(studentList);
-}
-
-/**
- * Refresh the table.
- * If the student is in the set, then assign class attribute "inSection"
- * Otherwise, "outSection"
- * @param studentList an array of DOM objects
- */
-async function refreshTable(studentList) {
-  if (studentList) {
-    var tdStudentDOM, student_info, tdStudentName, studentName;
-    var tr;
-    var classAttribute = 'unlock';
-    var inSection;
-    for (var i = 0, len = studentList.length; i < len; i++) {
-      inSection = false;
-      tr = studentList[i];
-      student_info = tr.getElementsByTagName("td");
-      if (student_info.length > 2) {
-        tdStudentDOM = student_info[1];
-        tdStudentName = tdStudentDOM.innerText;
-        studentName = getStudentName(tdStudentName);
-        inSection = isStudentInSection(studentName);
-        classAttribute = (lock) ? "locked " : "unlock ";
-        classAttribute += (inSection) ? "inSection" : "outSection";
-        if (disable)
-          tdStudentDOM.innerHTML = `<span>` + tdStudentName + `</span>`;
-        else
-          tdStudentDOM.innerHTML = `<span class=" ${classAttribute}">` + tdStudentName + `</span>`;
-        tr.style.display = (autoFilter && !inSection && !disable) ? 'none' : 'table-row';
-      }
-
-    }
-  }
-}
 /**
  * Check if the student is in the set
  * @param studentName format -> last name, first name
@@ -110,6 +36,75 @@ function isStudentInSection(studentName) {
 }
 
 /**
+ * Aite, Kuo
+ * Aite, Kuo (azkxxxx0000)
+ * Aite, Kuo (azk000)    try #1
+ * azk0103 (azk0103)
+ * The function should return Aite Kuo with all of the above examples.
+ * @return full name
+ */
+function getStudentName(tdStudentName) {
+  tdStudentName = tdStudentName.trim();
+  const studentName = tdStudentName.match(re);
+  return (studentName && studentName.length >= 2) ? studentName[1] : tdStudentName;
+}
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Automatically check off "Show grade to student?" if enabled.
+ * 
+ */
+async function autoCheckGrade() {
+  if (!autoShowGrade || disable) return;
+  let checkBox = null, attempts = 0;
+  while (checkBox = $('input#done'), checkBox.length == 0 && attempts++ < 500)
+    await timeout(10);
+  const isPressed = checkBox.attr('aria-pressed');
+  if (!isPressed)
+    checkBox.click();
+}
+/**
+ * Refresh the table.
+ * If the student is in the set, then assign class attribute "inSection"
+ * Otherwise, "outSection"
+ * @param studentList an array of DOM objects
+ */
+async function refreshTable() {
+  const form = $("#webcat_Form_3");
+  const formExist = form.length > 0;
+  if (formExist) {
+    let studentList = getStudentList();
+    const inCount = studentList.find('.inSection').length;
+    const outCount = studentList.find('.outSection').length;
+    const currentStatus = inCount + "-" + outCount + "-" + autoFilter + "-" + lock + "-" + disable;
+    if (studentList && lastStatus != currentStatus) {
+      let tdStudentDOM, student_info, tdStudentName, studentName, tr, inSection, classAttribute = 'unlock';
+      for (let i = 0, len = studentList.length; i < len; i++) {
+        inSection = false;
+        tr = studentList[i];
+        student_info = tr.getElementsByTagName("td");
+        if (student_info && student_info.length > 1) {
+          tdStudentDOM = student_info[1];
+          tdStudentName = tdStudentDOM.innerText;
+          studentName = getStudentName(tdStudentName);
+          inSection = isStudentInSection(studentName);
+          classAttribute = (lock) ? "locked " : "unlock ";
+          classAttribute += (inSection) ? "inSection" : "outSection";
+          if (disable)
+            tdStudentDOM.innerHTML = `<span>` + tdStudentName + `</span>`;
+          else
+            tdStudentDOM.innerHTML = `<span class=" ${classAttribute}">` + tdStudentName + `</span>`;
+          tr.style.display = (autoFilter && !inSection && !disable) ? 'none' : 'table-row';
+        }
+      }
+      lastStatus = currentStatus;
+    }
+  }
+}
+/**
  * When the user hits the student name on the table,
  * this function will handle it and reassign the DOM attribute.
  * Afterwards, it saves the new student name set to the cloud.
@@ -117,15 +112,12 @@ function isStudentInSection(studentName) {
  */
 function toggleStudent(student) {
   if (lock || disable) return;
-  var tdStudentName = student[0].innerText;
-  var studentName = getStudentName(tdStudentName);
-  var inSection = isStudentInSection(studentName);
-  var lockAttribute = (lock) ? "locked " : "unlock ";
-  var classAttribute = (inSection) ? "outSection" : "inSection";
-  if (inSection)
-    studentInSection.delete(studentName);
-  else
-    studentInSection.add(studentName);
+  const tdStudentName = student[0].innerText;
+  const studentName = getStudentName(tdStudentName);
+  const inSection = isStudentInSection(studentName);
+  const lockAttribute = (lock) ? "locked " : "unlock ";
+  const classAttribute = (inSection) ? "outSection" : "inSection";
+  (inSection) ? studentInSection.delete(studentName): studentInSection.add(studentName);
   // Put the data to the cloud
   chrome.storage.sync.set({
     studentInSection: Array.from(studentInSection)
@@ -133,9 +125,14 @@ function toggleStudent(student) {
     student.removeClass().addClass(lockAttribute).addClass(classAttribute);
   });
 }
-
+async function tableChangeListener(){
+  do {
+    refreshTable();
+    await timeout(250);
+  } while(true);
+}
 function setUp() {
-  var title = document.title;
+  const title = document.title;
   if (title.includes("View Submissions")){
     if($("textarea").length == 0)
       pageType = 1;
@@ -148,25 +145,27 @@ function setUp() {
   switch(pageType) {
     case 1:
       // Submission Page
+      // Bind click events
       $(document).on("click", "span.inSection, span.outSection", function() {
         toggleStudent($(this));
       });
       
+      // bind click event
       $(document).on("click", "a.active, input.icon", function() {
-        var form = $("#webcat_Form_3 tbody:nth-child(2)").html("");
-        oneTimeTableRefreshListener();
+        $("#webcat_Form_3 tbody:nth-child(2)").html("");
       });
+      // bind table change event
       $(document).on("change", "table", function() {
-        var form = $("#webcat_Form_3 tbody:nth-child(2)").html("");
-        oneTimeTableRefreshListener();
+        $("#webcat_Form_3 tbody:nth-child(2)").html("");
       });
 
       chrome.storage.sync.get({
         studentInSection: [],
       }, function(items) {
         items.studentInSection.reduce((s, e) => s.add(e), studentInSection);
-        oneTimeTableRefreshListener();
       });
+
+      tableChangeListener();
   		break;
   	case 2:
   	  // Grade Page
@@ -179,30 +178,43 @@ function setUp() {
    		break;
   }
 }
-
-async function removeLineNumber() {
-  var attempts = 0;
-  var count = 0;
-  var object;
+async function hideLineNumbers(){
+  let attempts = 0;
+  let lineNumbers;
   do {
-    object = $('iframe').contents().find('td.lineCount');
-    await timeout(10);
-  } while (object.length == 0 && attempts++ < 300);
-  if(lineNumber){
-    object.hide();
-  }
-  else
-    object.show();
-
-  do {
-    object = $('iframe').contents().find('pre.srcLine');
-    await timeout(10);
-  } while (object.length == 0 && attempts++ < 300);
-  object.each( function() {
+    lineNumbers = $('iframe').contents().find('td.lineCount');
+    await timeout(5);
+  } while (lineNumbers.length == 0 && attempts++ < 500);
+  (lineNumber) ? lineNumbers.hide() : lineNumbers.show();
+  // Remove indentation
+  lineNumbers.each( function() {
     if($(this).html().substr(0,6) == "&nbsp;")
       $(this).html($(this).html().substr(6));
   });
 }
+async function hideMessageBoxes(){
+  let attempts = 0;
+  let messageBoxes;
+  do {
+    messageBoxes = $('iframe').contents().find('td.messageBox');
+    await timeout(5);
+  } while (messageBoxes.length == 0 && attempts++ < 500);
+  (lineNumber) ? messageBoxes.hide() : messageBoxes.show();
+}
+async function removeSrcLines(){
+  let attempts = 0;
+  let srcLines;
+  do {
+    srcLines = $('iframe').contents().find('pre.srcLine');
+    await timeout(10);
+  } while (srcLines.length == 0 && attempts++ < 300);
+}
+function removeLineNumber() {
+  hideLineNumbers();
+  hideMessageBoxes();
+  removeSrcLines();
+}
+
 $(document).ready(function() {
   chrome.storage.sync.get({
     autoFilter: false,
@@ -217,7 +229,6 @@ $(document).ready(function() {
     autoShowGrade = items.autoShowGrade;
     lineNumber = items.lineNumber;
     setUp();
-    oneTimeTableRefreshListener();
   });
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -227,7 +238,7 @@ $(document).ready(function() {
       autoShowGrade = request.autoShowGrade;
       lineNumber = request.lineNumber;
       if (pageType == 1)
-        oneTimeTableRefreshListener();
+        ;
       else if (pageType == 2)
         autoCheckGrade();
       else if (pageType == 3)
